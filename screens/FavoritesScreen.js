78791e37d44ebae96e918081468a3f15504cc64a
Adilson Souza <adilsonsouzadocarmojf@hotmail.com>
2020-01-20 14:48:09 -0300

    Adicionando tela padrão quando não tivermos favoritos

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -1,17 +1,28 @@
 import React from 'react';
-import { Dimensions, StyleSheet } from 'react-native';
+import { Dimensions, StyleSheet, View } from 'react-native';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 import { useSelector } from 'react-redux';
 
 import GridOrList from '../components/GridOrList';
 import CustomHeaderButton from '../components/CustomHeaderButton';
+import DefaultStyle from '../constants/style';
+import DefaultText from '../components/DefaultText';
 
 const FavoritesScreen = props => {
   const favorites = useSelector(state => state.meals.favoriteMeals);
+
   const nav = (itemData) => {
     const isFavorite = favorites.some(ml => ml.id === itemData.item.id);
     props.navigation.navigate({ routeName: 'MealDetail', params: { meal: itemData.item, isFavorite: isFavorite } })
   }
+
+
+  if (favorites.length === 0 || !favorites)
+    return (
+      <View style={DefaultStyle.screen}>
+        <DefaultText>No favorite meals found.</DefaultText>
+      </View>
+    )
 
   return (
     <GridOrList
